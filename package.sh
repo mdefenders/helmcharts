67ef4460c5082e53fff34dae229d334c04d6185c
den <aditor@gmail.com>
2025-08-06 22:02:21 +0300

    Publish Helm chart

--- package.sh
+++ package.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 helm package argo-appsets
-repo index . --url  https://mdefenders.github.io/helmcharts
+helm repo index . --url  https://mdefenders.github.io/helmcharts
 git add .
 git commit -m "Publish Helm chart"
 git push origin gh-pages

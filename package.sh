bba7fcb216382b41040fcb36735880c8ff28c5e9
den <aditor@gmail.com>
2025-08-07 06:48:39 +0300

    cleanup

--- package.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-helm package charts/argo-appsets charts/it-delivers
-helm repo index . --url  https://mdefenders.github.io/helmcharts
-git add .
-git commit -m "Publish Helm chart"
-git push origin gh-pages
